140c1ef57702ab64458b4fe5194160ce6706bac2
Khang Le <lekhang4497@gmail.com>
2018-10-27 18:28:06 +0700

    Updates

---
+++ precache-manifest.caef77030cd00d868fb50bafe83dcdd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "041b1e5c599771b7960d",
+    "url": "/ghem-website/static/js/runtime~main.041b1e5c.js"
+  },
+  {
+    "revision": "a4a44c696fba33700c6a",
+    "url": "/ghem-website/static/js/main.a4a44c69.chunk.js"
+  },
+  {
+    "revision": "405845da122cb1b8037c",
+    "url": "/ghem-website/static/js/1.405845da.chunk.js"
+  },
+  {
+    "revision": "a4a44c696fba33700c6a",
+    "url": "/ghem-website/static/css/main.de8f17a5.chunk.css"
+  },
+  {
+    "revision": "405845da122cb1b8037c",
+    "url": "/ghem-website/static/css/1.221361d5.chunk.css"
+  },
+  {
+    "revision": "b016881e32333112322d321748480718",
+    "url": "/ghem-website/index.html"
+  }
+];
