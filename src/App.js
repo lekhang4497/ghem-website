2d26427472b18150f6c841079077f3c3990f9f40
Khang Le <lekhang4497@gmail.com>
2018-10-28 13:45:45 +0700

    fix bug refresh page with hashrouter

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, {Component} from 'react';
 import Root from "./components/Root";
 import 'antd/dist/antd.css';
 import "./App.css"
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, HashRouter, Route} from "react-router-dom";
 import MenuPage from "./components/MenuPage";
 import ScrollToTop from "./components/ScrollToTop";
 
 class App extends Component {
     render() {
         return (
-            <BrowserRouter>
+            <HashRouter>
                 <ScrollToTop>
                     <Route exact path="/" component={Root}/>
                     <Route path="/menu" component={MenuPage}/>
                 </ScrollToTop>
-            </BrowserRouter>
+            </HashRouter>
         );
     }
 }
--- src/components/Root.js
+++ src/components/Root.js
@@ -18,6 +18,10 @@ class Root extends Component {
         }
     }
 
+    componentDidMount() {
+        window.scrollTo(0, 0);
+    }
+
     render() {
         return (
             <div>
